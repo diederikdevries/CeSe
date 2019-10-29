9fdd14ecf1f116290179d68b6074b129189da789
Diederik de Vries <github@diederik.nl>
2019-10-29 11:44:29 +0100

    Diederik: created some scratchfiles with old code. The current code is gory, and works somewhat.
    Added: Excel import. We have a function to read and return all values on a given row.
    Next up: working with dictionaries. We want to create a student, and add the modules he recieved

--- check.py
+++ check.py
@@ -1,12 +1,28 @@
-# Inlezen ledenlijst zoals aangeleverd
+from openpyxl import load_workbook
+import glob
 
-with open('klas_a.txt') as a:
-    ledenlijst = a.read().splitlines()
+# TO-DO-List:
+# TODO: Fix variable-names to normal standards
+# All English speaking people should be able to read it ;)
 
-with open('certified secure.txt') as b:
-    ledenlijst_website = b.read().splitlines()
+# This Excel file contains all information on all students...
+csreport = 'report.e88414.01-10-2019.xlsx'
 
+# Import the Excel to a Workbook...
+wb = load_workbook(filename = csreport)
+sheet_ranges = wb['Overview']
 
-for lid in ledenlijst:
-    if lid not in ledenlijst_website:
-        print lid + " Heeft CS nog niet gehaald."
+def readrow(rownr):
+    mylist = []
+    for row in sheet_ranges.iter_cols(min_col=rownr, max_col=rownr, min_row=2, max_row=None, values_only=False):
+        for cell in row:
+            mylist.append(cell.value)
+    return mylist
+
+# Lets create students...
+
+allstudents = readrow(0)
+
+for students in allstudents:
+    student = {'email': students[]}
+
